63558a7f3233ecd49ad8b0f7d8df5c4efacef96d
panwenye <lsjwzh@163.com>
2015-01-20 00:05:24 +0800

    初步完成缩放效果

--- app/src/main/java/com/lsjwzh/widget/recyclerviewpagerdeomo/LayoutFragment.java
+++ app/src/main/java/com/lsjwzh/widget/recyclerviewpagerdeomo/LayoutFragment.java
@@ -96,13 +96,28 @@ public class LayoutFragment extends Fragment {
             public void onScrolled(RecyclerViewEx recyclerView, int i, int i2) {
 //                mPositionText.setText("First: " + mRecyclerView.getFirstVisiblePosition());
                 int childCount = mRecyclerView.getChildCount();
+                int width = mRecyclerView.getChildAt(0).getWidth();
+                int padding  = (mRecyclerView.getWidth() - width)/2;
                 mCountText.setText("Count: " + childCount);
                 for (int j = 0; j < childCount; j++) {
                     View v = mRecyclerView.getChildAt(j);
-                    if(v.getLeft()<100){
-                        float scale = (100 - v.getLeft())/200f;
-//                        v.setScaleX(scale+0.5f);
-//                        v.setScaleY(scale+0.5f);
+                    //往左 从 padding 到 -(v.getWidth()-padding) 的过程中，由大到小
+                    float rate = 0;
+                    if(v.getLeft()<=padding){
+                        if(v.getLeft()>=padding-v.getWidth()){
+                            rate = (padding - v.getLeft())*1f/v.getWidth();
+                        }else {
+                            rate = 1;
+                        }
+                        v.setScaleX(1-rate*0.1f);
+                        v.setScaleY(1-rate*0.1f);
+                    }else{
+                        //往右 从 padding 到 recyclerView.getWidth()-padding 的过程中，由大到小
+                        if(v.getLeft()<=recyclerView.getWidth()-padding){
+                            rate = (recyclerView.getWidth()-padding - v.getLeft())*1f/v.getWidth();
+                        }
+                        v.setScaleX(0.9f+rate*0.1f);
+                        v.setScaleY(0.9f+rate*0.1f);
                     }
                 }
             }
