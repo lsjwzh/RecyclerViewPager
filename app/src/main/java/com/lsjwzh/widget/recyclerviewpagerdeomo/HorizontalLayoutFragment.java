f18c7737f300fe759bbc7dd50b02cf7353b8bb9e
lsjwzh <lsjwzh@163.com>
2015-07-02 09:03:22 +0800

    update demo

--- app/src/main/java/com/lsjwzh/widget/recyclerviewpagerdeomo/HorizontalLayoutFragment.java
+++ app/src/main/java/com/lsjwzh/widget/recyclerviewpagerdeomo/HorizontalLayoutFragment.java
@@ -103,12 +103,15 @@ public class HorizontalLayoutFragment extends Fragment {
                             rate = 1;
                         }
                         v.setScaleY(1 - rate * 0.1f);
+                        v.setScaleX(1 - rate * 0.1f);
+
                     } else {
                         //往右 从 padding 到 recyclerView.getWidth()-padding 的过程中，由大到小
                         if (v.getLeft() <= recyclerView.getWidth() - padding) {
                             rate = (recyclerView.getWidth() - padding - v.getLeft()) * 1f / v.getWidth();
                         }
                         v.setScaleY(0.9f + rate * 0.1f);
+                        v.setScaleX(0.9f + rate * 0.1f);
                     }
                 }
             }
@@ -119,17 +122,26 @@ public class HorizontalLayoutFragment extends Fragment {
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
                 if (mRecyclerView.getChildCount() < 3) {
                     if (mRecyclerView.getChildAt(1) != null) {
-                        View v1 = mRecyclerView.getChildAt(1);
-                        v1.setScaleY(0.9f);
+                        if(mRecyclerView.getCurrentPosition()==0) {
+                            View v1 = mRecyclerView.getChildAt(1);
+                            v1.setScaleY(0.9f);
+                            v1.setScaleX(0.9f);
+                        } else {
+                            View v1 = mRecyclerView.getChildAt(0);
+                            v1.setScaleY(0.9f);
+                            v1.setScaleX(0.9f);
+                        }
                     }
                 } else {
                     if (mRecyclerView.getChildAt(0) != null) {
                         View v0 = mRecyclerView.getChildAt(0);
                         v0.setScaleY(0.9f);
+                        v0.setScaleX(0.9f);
                     }
                     if (mRecyclerView.getChildAt(2) != null) {
                         View v2 = mRecyclerView.getChildAt(2);
                         v2.setScaleY(0.9f);
+                        v2.setScaleX(0.9f);
                     }
                 }
 
