b0348d105ca6338752e38e69c19329e5654ff800
François Colin <fcolin@ecocea.com>
2016-07-06 15:32:00 +0200

    Fixes #102 - Don't spam syserr when using a holder without mPosition field

--- lib/src/main/java/com/lsjwzh/widget/recyclerviewpager/LoopRecyclerViewPagerAdapter.java
+++ lib/src/main/java/com/lsjwzh/widget/recyclerviewpager/LoopRecyclerViewPagerAdapter.java
@@ -1,11 +1,14 @@
 package com.lsjwzh.widget.recyclerviewpager;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import java.lang.reflect.Field;
 
 public class LoopRecyclerViewPagerAdapter<VH extends RecyclerView.ViewHolder>
         extends RecyclerViewPagerAdapter<VH> {
+
+    private static final String TAG = LoopRecyclerViewPager.class.getSimpleName();
 
     private Field mPositionField;
 
@@ -44,19 +47,21 @@ public class LoopRecyclerViewPagerAdapter<VH extends RecyclerView.ViewHolder>
     public void onBindViewHolder(VH holder, int position) {
         super.onBindViewHolder(holder, getActualPosition(position));
         // because of getCurrentPosition may return ViewHolder‘s position,
-        // so we must reset mPosition.
+        // so we must reset mPosition if exists.
         if (mPositionField == null) {
             try {
                 mPositionField = holder.getClass().getDeclaredField("mPosition");
                 mPositionField.setAccessible(true);
             } catch (NoSuchFieldException e) {
-                e.printStackTrace();
+                Log.i(TAG, "The holder doesn't have a mPosition field.");
             }
         }
-        try {
-            mPositionField.set(holder, position);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (mPositionField != null) {
+            try {
+                mPositionField.set(holder, position);
+            } catch (Exception e) {
+                Log.w(TAG, "Error while updating holder's mPosition field", e);
+            }
         }
     }
 
